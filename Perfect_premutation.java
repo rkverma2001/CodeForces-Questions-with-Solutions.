f3fff5fcc55166186d9498a6ca41ea5d22299a07
Ritesh Kumar <riteshkumarverma2018@gmail.com>
2023-05-23 08:01:29 +0530

    Perfect Permutation Completed

--- Perfect_premutation.java
+++ Perfect_premutation.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
+
 public class Perfect_premutation {
 
-    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        if (n % 2 == 1) {
+            System.out.println(-1);
+        }
+
+        else {
+            int[] permutation = new int[n];
+            int i = 0;
+            for (int j = 2; j <= n; j += 2) {
+                permutation[i++] = j;
+                permutation[i++] = j - 1;
+            }
+
+            for (int j = 0; j < permutation.length; j++) {
+                System.out.println(permutation[j] + " ");
+            }
+        }
+    }
 }
